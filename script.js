9acce72854e4a3af3fc1d13e551bebc97b67846c
kateFrontend <ecdanilova@gmail.com>
2023-02-22 10:19:29 -0500

    setup script

---
+++ script.js
@@ -0,0 +1,22 @@
+const counters = document.querySelectorAll('.counter')
+
+counters.forEach(counter => {
+    counter.innerText = '0'
+
+    const updateCounter = () => {
+        const target = +counter.getAttribute('data-target')  // + to change the type from string to number console.log(typeof target, target)
+
+        const c = +counter.innerText
+
+        const increment = target / 300  // console.log(increment)
+
+        if(c < target){
+            counter.innerText = `${Math.ceil(c + increment)}`
+            setTimeout(updateCounter, 1)
+        }else {
+            counter.innerText = target
+        }
+    }
+
+    updateCounter()
+})
